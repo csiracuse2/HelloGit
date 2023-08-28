52faedcc99e6206d8904dd032f7030d05a5e23a0
colli <colli@LAPTOP-5LJ8DCEC.unl.edu>
2023-08-28 09:29:49 -0500

    Initial Commit

---
+++ src/hello.java
@@ -0,0 +1,15 @@
+
+public class hello {
+
+	public static void main (String args[])
+	{
+		/*
+		 * Collin Siracuse
+		 * 8/28/23
+		 * A program that prints name and major
+		 * 
+		 * 
+		 */
+		System.out.println("Name: Collin!");		
+	}
+}
